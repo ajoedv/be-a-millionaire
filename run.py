fc179462081e6f4b96e8388f47c761f4a4ab471a
Amjad <mamered@gmail.com>
2023-05-18 17:53:15 +0000

    add money_increments list to define money increment pattern

--- run.py
+++ run.py
@@ -54,6 +54,7 @@ def game_loop():
 
 
     # Define the money increment pattern
+    money_increments = [100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000]
 
 
     # Function for checking the answer
