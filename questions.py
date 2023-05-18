faff91cf13c76e804f57cde62c13efd1cb440924
Amjad <mamered@gmail.com>
2023-05-18 23:38:36 +0000

    add the final questions to the questions module

--- questions.py
+++ questions.py
@@ -1,93 +1,127 @@
-# Define a list of questions with their corresponding answers
 questions = [
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "question": "What is the largest organ in the human body?",
+        "options": ["a. Liver", "b. Heart", "c. Skin", "d. Brain"],
         "answer": "c"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Which instrument has 88 keys?",
+        "options": ["a. Piano", "b. Violin", "c. Guitar", "d. Trumpet"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "What is the national bird of the United States?",
+        "options": ["a. Bald Eagle", "b. Peacock", "c. Sparrow", "d. Penguin"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Who is the founder of Microsoft?",
+        "options": ["a. Bill Gates", "b. Steve Jobs", "c. Mark Zuckerberg", "d. Elon Musk"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Which continent is the largest by land area?",
+        "options": ["a. Asia", "b. Africa", "c. North America", "d. Europe"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "What is the chemical symbol for oxygen?",
+        "options": ["a. O2", "b. O", "c. H2O", "d. CO2"],
+        "answer": "b"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Which country is famous for the Great Wall?",
+        "options": ["a. China", "b. India", "c. Mexico", "d. Italy"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Who wrote the book 'Harry Potter and the Philosopher's Stone'?",
+        "options": ["a. J.K. Rowling", "b. Roald Dahl", "c. C.S. Lewis", "d. Stephenie Meyer"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "What is the largest ocean in the world?",
+        "options": ["a. Pacific Ocean", "b. Atlantic Ocean", "c. Indian Ocean", "d. Arctic Ocean"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Which country is known for the Taj Mahal?",
+        "options": ["a. India", "b. China", "c. Brazil", "d. Egypt"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "What is the symbol for the chemical element iron?",
+        "options": ["a. Fe", "b. Hg", "c. Au", "d. Ag"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Who is the author of 'To Kill a Mockingbird'?",
+        "options": ["a. Harper Lee", "b. J.R.R. Tolkien", "c. George Orwell", "d. Ernest Hemingway"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Which city is known as the 'Big Apple'?",
+        "options": ["a. New York City", "b. Los Angeles", "c. Chicago", "d. San Francisco"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "What is the largest desert in the world?",
+        "options": ["a. Sahara Desert", "b. Gobi Desert", "c. Kalahari Desert", "d. Atacama Desert"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Who is the current President of the United States?",
+        "options": ["a. Joe Biden", "b. Barack Obama", "c. Donald Trump", "d. George W. Bush"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Which country is known for the Great Barrier Reef?",
+        "options": ["a. Australia", "b. Brazil", "c. Canada", "d. Germany"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "Who invented the telephone?",
+        "options": ["a. Alexander Graham Bell", "b. Thomas Edison", "c. Isaac Newton", "d. Albert Einstein"],
+        "answer": "a"
     },
     {
-        "question": "What is the color of the sky?",
-        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
-        "answer": "c"
+        "question": "What is the capital of France?",
+        "options": ["a. Paris", "b. London", "c. Rome", "d. Madrid"],
+        "answer": "a"
     },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["a. Mars", "b. Jupiter", "c. Venus", "d. Saturn"],
+        "answer": "a"
+    },
+    {
+        "question": "Who painted the Mona Lisa?",
+        "options": ["a. Leonardo da Vinci", "b. Vincent van Gogh", "c. Pablo Picasso", "d. Michelangelo"],
+        "answer": "a"
+    },
+    {
+        "question": "Which country is known for its pyramids?",
+        "options": ["a. Egypt", "b. Brazil", "c. China", "d. Australia"],
+        "answer": "a"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "options": ["a. Ag", "b. Au", "c. Fe", "d. Hg"],
+        "answer": "b"
+    },
+    {
+        "question": "Who wrote the play 'Romeo and Juliet'?",
+        "options": ["a. William Shakespeare", "b. Ernest Hemingway", "c. Mark Twain", "d. Jane Austen"],
+        "answer": "a"
+    },
+    {
+        "question": "Which animal is known as the 'King of the Jungle'?",
+        "options": ["a. Lion", "b. Elephant", "c. Tiger", "d. Giraffe"],
+        "answer": "a"
+    },
+    {
+        "question": "What is the tallest mountain in the world?",
+        "options": ["a. Mount Everest", "b. K2", "c. Mount Kilimanjaro", "d. Mount Fuji"],
+        "answer": "a"
+    }
 ]
