3cbc63f68b392394d36b4c4fe3bfe716af056e53
Amjad <mamered@gmail.com>
2023-05-18 17:39:39 +0000

    Define temporary questions and answers dictionary in question module

---
+++ questions.py
@@ -0,0 +1,93 @@
+# Define a list of questions with their corresponding answers
+questions = [
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+    {
+        "question": "What is the color of the sky?",
+        "options": ["a) Red", "b) Green", "c) Blue", "d) Pink"],
+        "answer": "c"
+    },
+]
