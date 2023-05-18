8154f0579288206a121d59317eb55e59a5606b11
Amjad <mamered@gmail.com>
2023-05-18 16:53:59 +0000

    Add basic function structure

--- run.py
+++ run.py
@@ -1,2 +1,50 @@
 import random
 import questions
+
+# Get player name
+
+
+# welcoming
+
+
+
+# Game rules
+
+
+# Game loop
+def game_loop():
+    # Initialize variables
+
+
+    # Shuffle the questions
+
+
+    # Define the money increment pattern
+
+
+    # Function for checking the answer
+
+
+    # Function for getting user's answer
+
+
+    # Function for handling a single question
+
+
+        # Get user's answer
+
+
+        # Check if the answer is correct
+
+
+    # Game loop
+
+
+    # Game ended
+
+
+    # Ask if the player wants to play again
+
+
+
+game_loop()
